0f483905a5cf1099a93eb74e3252f37c138c89c4
Puneet Patel <Puneet070500@gmail.com>
2024-04-18 14:25:35 +0530

    Create components

---
+++ 02 create-react-component/src/App.jsx
@@ -0,0 +1,11 @@
+import Button from "./Button";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <Button />
+    </>
+  );
+}
+
+export default App;
---
+++ 02 create-react-component/src/Button.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Button = () => {
+  return <button>Lets do it.</button>;
+};
+
+export default Button;
